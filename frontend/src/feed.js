a85ce83dc51c8461d369cd3b9834ea3f4f9e7d14
Dion Earle <dion.earle@gmail.com>
2019-08-09 14:09:29 +1000

    Implemented the feed for logged in users (1hr)

--- frontend/src/feed.js
+++ frontend/src/feed.js
@@ -2,13 +2,20 @@ import API_URL from './backend_url.js';
 
 export default function feed(feed) {
 
-    // if localstorage.getItem(token) isn't set,
-    // then call a method to fetch from GET/post/public
+    // here we retrieve the token from local storage
+    const token = localStorage.getItem('token');
 
-    // if the token does exist,
-    // then we fetch from GET /user/feed
+    // if the token isn't set, this means the user isn't
+    // logged in, so we display the public feed
+    if (token === null) {
+        setupPublicFeed(feed);
+    // if the token is set, we display this user's feed
+    } else {
+        setupUserFeed(feed, token);
+    }
+}
 
-    // console.log(localStorage.getItem('token');
+function setupPublicFeed(feed) {
 
     // we also setup the options for our fetch request
     const options = {
@@ -25,7 +32,34 @@ export default function feed(feed) {
 
         const posts = response.posts;
 
-        // TODO: posts should be in reverse chronological order
+        for (let i = 0; i < posts.length; i++) {
+
+            const post = setupPostHTML(posts[i]);
+
+            feed.appendChild(post);
+        }
+
+    });
+}
+
+function setupUserFeed(feed, token) {
+
+    // we also setup the options for our fetch request
+    const options = {
+        method: 'GET',
+        headers: {
+            'Authorization': `Token ${token}`,
+            'Content-Type': 'application/json'
+        }
+    }
+
+    // we use auth/login to authenticate this login
+    fetch(`${API_URL}/user/feed`, options)
+    .then(response =>  response.json())
+    .then(response => {
+
+        const posts = response.posts;
+
         for (let i = 0; i < posts.length; i++) {
 
             const post = setupPostHTML(posts[i]);
@@ -63,12 +97,20 @@ function setupPostHTML(json) {
     postTitle.setAttribute('data-id-title', "");       
     postTitle.classList.add('post-title', 'alt-text');   
     postTitle.textContent = json.title;  
-    content.appendChild(postTitle);
+    content.appendChild(postTitle); 
 
     // the description text should also be shown
     const postDescription = document.createElement('p');         
     postDescription.textContent = json.text; 
     content.appendChild(postDescription);
+
+    // if there is an image, we want to display this as well
+    if (json.image !== null) {
+        const postImage = document.createElement('img'); 
+        postImage.classList.add('post-image'); 
+        postImage.src = 'data:image/png;base64, ' + json.image;
+        content.appendChild(postImage);
+    }
 
     // the content element also contains the post author
     const postAuthor = document.createElement('p');
@@ -93,31 +135,24 @@ function setupPostHTML(json) {
     postSubSeddit.textContent = '/s/' + json.meta.subseddit;
     content.appendChild(postSubSeddit);
 
-    // TODO: if there is an image, we want to display this as well
-    /*if (json.image !== null) {
-        const postImage = document.createElement('data::image/jpeg;base64, img');  
-        postImage.src = json.image;
-        content.appendChild(postImage);
-    } */
-
     return post;
 }
 
 function setPostTime(postTime) {
 
     const postDate = new Date(postTime * 1000);
-    const day = postDate.getDay();
     const month = postDate.getMonth();
     const date = postDate.getDate();
     const year = postDate.getFullYear();
 
     let hours = postDate.getHours();
+    const suffix = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
     hours = hours ? hours : 12;
-    const suffix = hours >= 12 ? 'PM' : 'AM';
     const minutes = postDate.getMinutes();
     const seconds = postDate.getSeconds();
     
     return 'Posted on ' + date + '/' + month + '/' + year 
-    + ' at ' + hours + ':' + minutes + ':' + seconds + suffix;
+    + ' at ' + hours + ':' + ("0"+minutes).slice(-2) + ':' 
+    + ("0"+seconds).slice(-2) + suffix;
 }
