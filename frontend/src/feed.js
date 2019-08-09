d75cd9ebf05e8b503c8dae5f94bb7f0cae728b5b
Dion Earle <dion.earle@gmail.com>
2019-08-09 13:15:47 +1000

    Implemented the feed for non-logged in users (1hr)

--- frontend/src/feed.js
+++ frontend/src/feed.js
@@ -1,23 +1,45 @@
+import API_URL from './backend_url.js';
+
 export default function feed(feed) {
 
- /*  fetch('../data/feed.json')
-    .then((response) => response.json())
-    .then(response => {       
-        // comment
-        const posts = response.posts;
-        // NOTE: posts should be in reverse chronological order
-        for (let i = 0; i < posts.length; i++) {
-            console.log(posts);
-            //const post = setupPostHTML(response[i]);
-            // feed.appendChild(post);
-        }
-    }); */
+    // if localstorage.getItem(token) isn't set,
+    // then call a method to fetch from GET/post/public
 
-    //fetch('test').then(r=>r.text()).then(r => console.log(r));
+    // if the token does exist,
+    // then we fetch from GET /user/feed
+
+    // console.log(localStorage.getItem('token');
+
+    // we also setup the options for our fetch request
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    }
+
+    // we use auth/login to authenticate this login
+    fetch(`${API_URL}/post/public`, options)
+    .then(response =>  response.json())
+    .then(response => {
+
+        const posts = response.posts;
+
+        // TODO: posts should be in reverse chronological order
+        for (let i = 0; i < posts.length; i++) {
+
+            const post = setupPostHTML(posts[i]);
+
+            feed.appendChild(post);
+        }
+
+    });
 }
 
 // simply sets up the HTML for the given post object
 function setupPostHTML(json) {
+
+    const feed = document.getElementById('feed');
 
     // here we create the post element
     const post = document.createElement('li');
@@ -52,31 +74,50 @@ function setupPostHTML(json) {
     const postAuthor = document.createElement('p');
     postAuthor.setAttribute('data-id-author', "");       
     postAuthor.classList.add('post-author');   
-    postAuthor.textContent = json.meta.author; 
+    postAuthor.textContent = 'Posted by @' + json.meta.author; 
     content.appendChild(postAuthor);
 
     // the content element shows the time of the post
     const postTime = document.createElement('p');         
-    postTime.textContent = Date.now() - json.meta.published; 
+    postTime.textContent = setPostTime(json.meta.published);
     content.appendChild(postTime);
 
     // the number of comments should be displayed
     const postComments = document.createElement('p');         
-    postComments.textContent = json.comments.length;
+    postComments.textContent = 'Comments: ' + json.comments.length;
     content.appendChild(postComments);
 
     // we also want to display the subseddit this post
     // belongs to
     const postSubSeddit = document.createElement('p');         
-    postSubSeddit.textContent = json.metadata.subseddit;
+    postSubSeddit.textContent = '/s/' + json.meta.subseddit;
     content.appendChild(postSubSeddit);
 
-    // if there is an image, we want to display this as well
-    if (json.image !== null) {
+    // TODO: if there is an image, we want to display this as well
+    /*if (json.image !== null) {
         const postImage = document.createElement('data::image/jpeg;base64, img');  
         postImage.src = json.image;
         content.appendChild(postImage);
-    }
+    } */
 
     return post;
+}
+
+function setPostTime(postTime) {
+
+    const postDate = new Date(postTime * 1000);
+    const day = postDate.getDay();
+    const month = postDate.getMonth();
+    const date = postDate.getDate();
+    const year = postDate.getFullYear();
+
+    let hours = postDate.getHours();
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    const suffix = hours >= 12 ? 'PM' : 'AM';
+    const minutes = postDate.getMinutes();
+    const seconds = postDate.getSeconds();
+    
+    return 'Posted on ' + date + '/' + month + '/' + year 
+    + ' at ' + hours + ':' + minutes + ':' + seconds + suffix;
 }
